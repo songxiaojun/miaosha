0b2c13d287551b6e2dccc7ef45e952a1ded8b683
sj <1215817408@qq.com>
2018-08-06 00:15:35 +0800

    sj

---
+++ src/main/java/com/hfuu/miaosha/service/GoodsService.java
@@ -0,0 +1,16 @@
+package com.hfuu.miaosha.service;
+
+import com.hfuu.miaosha.vo.GoodsVo;
+
+import java.util.List;
+
+public interface GoodsService {
+    public List<GoodsVo> listGoodsVo();
+
+    public GoodsVo getGoodsVoByGoodsId(long goodsId);
+
+    public boolean reduceStock(GoodsVo goods);
+
+    public void resetStock(List<GoodsVo> goodsList);
+    }
+}
---
+++ src/main/java/com/hfuu/miaosha/service/impl/GoodsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.hfuu.miaosha.service.impl;
+
+import com.hfuu.miaosha.service.GoodsService;
+import com.hfuu.miaosha.vo.GoodsVo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class GoodsServiceImpl implements GoodsService {
+    @Override
+    public List<GoodsVo> listGoodsVo() {
+        return null;
+    }
+
+    @Override
+    public GoodsVo getGoodsVoByGoodsId(long goodsId) {
+        return null;
+    }
+
+    @Override
+    public boolean reduceStock(GoodsVo goods) {
+        return false;
+    }
+
+    @Override
+    public void resetStock(List<GoodsVo> goodsList) {
+
+    }
+}
