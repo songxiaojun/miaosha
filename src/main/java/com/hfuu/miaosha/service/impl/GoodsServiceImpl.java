ee9a2c104160555ab50d713c06d029a264903157
sj <1215817408@qq.com>
2018-08-06 23:27:01 +0800

    sj

--- src/main/java/com/hfuu/miaosha/service/impl/GoodsServiceImpl.java
+++ src/main/java/com/hfuu/miaosha/service/impl/GoodsServiceImpl.java
@@ -1,15 +1,20 @@
 package com.hfuu.miaosha.service.impl;
 
+import com.hfuu.miaosha.dao.GoodsDao;
 import com.hfuu.miaosha.service.GoodsService;
 import com.hfuu.miaosha.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class GoodsServiceImpl implements GoodsService {
+
+    @Autowired
+    private GoodsDao goodsDao;
     @Override
     public List<GoodsVo> listGoodsVo() {
-        return null;
+        return goodsDao.listGoodsVo();
     }
 
     @Override
